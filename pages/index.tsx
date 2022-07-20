c67d9b1b098ca85c05628e92d17575c7ae283ec1
h0ngcha0 <lbs.lhc@gmail.com>
2022-07-20 02:30:10 +0200

    Update greeter btc price

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import GreeterJson from '../artifacts/greeter/greeter.ral.json'
+import UpdateBtcPriceJson from '../artifacts/greeter/update_btc_price.ral.json'
 import addresses from '../configs/addresses.json'
-import { Contract, NodeProvider } from '@alephium/web3'
+import { binToHex, Contract, contractIdFromAddress, NodeProvider, Script } from '@alephium/web3'
 import { useEffect, useState } from 'react'
+import { testAddress1, testWallet1 } from '../test/signers'
 
 const Home: NextPage = () => {
   const nodeProvider = new NodeProvider('http://127.0.0.1:22973')
   const [btcPrice, setBtcPrice] = useState<number | undefined>(undefined)
+  const [formBtcPrice, updateFormBtcPrice] = useState<number | undefined>(undefined)
   const GreeterContract = Contract.fromJson(GreeterJson)
+  const UpdateBtcPriceScript = Script.fromJson(UpdateBtcPriceJson)
 
   useEffect(() => {
     (async () => {
@@ -19,6 +23,24 @@ const Home: NextPage = () => {
       console.debug(`error fetching state for greeter`, e)
     })
   })
+
+  async function updateBtcPrice() {
+    if (formBtcPrice) {
+      console.log('newPrice', formBtcPrice)
+      const signer = await testWallet1(nodeProvider)
+      const deployParams = await UpdateBtcPriceScript.paramsForDeployment({
+        signerAddress: testAddress1,
+        initialFields: {
+          newPrice: +formBtcPrice,
+          greeterContractId: binToHex(contractIdFromAddress(addresses.greeterContractAddress))
+        }
+      })
+
+      await signer.signExecuteScriptTx(deployParams)
+    } else {
+      console.log('no new price')
+    }
+  }
 
   return (
     <div className={styles.container}>
@@ -38,6 +60,19 @@ const Home: NextPage = () => {
             btcPrice ? <span>BTC Price: {btcPrice}</span> : <span>BTC Price Unknown</span>
           }
         </p>
+
+        <div>
+          <input
+            type="number"
+            min="0"
+            placeholder="New BTC Price"
+            className="mt-8 border rounded p-4"
+            onChange={e => updateFormBtcPrice(e.target.value)}
+          />
+          <button onClick={updateBtcPrice} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+            Update BTC Price
+          </button>
+        </div>
       </main>
     </div>
   )
--- test/greeter/greeter.test.ts
+++ test/greeter/greeter.test.ts
@@ -1,4 +1,4 @@
-import { testWallet1 } from '../signers'
+import { testAddress1, testWallet1 } from '../signers'
 
 import { NodeProvider, Contract, Script, TestContractParams } from '@alephium/web3'
 
@@ -7,18 +7,19 @@ describe('greeter contract', function() {
     const provider = new NodeProvider('http://127.0.0.1:22973')
     const greeter = await Contract.fromSource(provider, 'greeter/greeter.ral')
 
+    const oldPrice = 1000
     const testParams: TestContractParams = {
-      initialFields: { btcPrice: 1 }
+      initialFields: { btcPrice: oldPrice }
     }
     const testResult = await greeter.testPublicMethod(provider, 'greet', testParams)
-    expect(testResult.returns).toEqual([1])
+    expect(testResult.returns).toEqual([oldPrice])
     expect(testResult.contracts[0].codeHash).toEqual(greeter.codeHash)
-    expect(testResult.contracts[0].fields.btcPrice).toEqual(1)
+    expect(testResult.contracts[0].fields.btcPrice).toEqual(oldPrice)
 
     const signer = await testWallet1(provider)
 
     const deployTx = await greeter.transactionForDeployment(signer, {
-      initialFields: { btcPrice: 1 },
+      initialFields: { btcPrice: oldPrice },
       initialTokenAmounts: []
     })
     expect(deployTx.fromGroup).toEqual(0)
@@ -27,6 +28,9 @@ describe('greeter contract', function() {
     expect(submitResult.fromGroup).toEqual(0)
     expect(submitResult.toGroup).toEqual(0)
     expect(submitResult.txId).toEqual(deployTx.txId)
+
+    const oldState = await greeter.fetchState(provider, deployTx.contractAddress, 0)
+    expect(oldState.fields.btcPrice).toEqual(oldPrice)
 
     const greeterContractId = deployTx.contractId
     const main = await Script.fromSource(provider, 'greeter/greeter_main.ral')
@@ -39,5 +43,20 @@ describe('greeter contract', function() {
     const mainSubmitResult = await signer.submitTransaction(mainScriptTx.unsignedTx, mainScriptTx.txId)
     expect(mainSubmitResult.fromGroup).toEqual(0)
     expect(mainSubmitResult.toGroup).toEqual(0)
+
+    // Update BTC price
+    const newPrice = 30000
+    const updateBtcPriceScript = await Script.fromSource(provider, 'greeter/update_btc_price.ral')
+    const deployParams = await updateBtcPriceScript.paramsForDeployment({
+      signerAddress: testAddress1,
+      initialFields: {
+        newPrice: newPrice,
+        greeterContractId: greeterContractId
+      }
+    })
+    await signer.signExecuteScriptTx(deployParams)
+
+    const newState = await greeter.fetchState(provider, deployTx.contractAddress, 0)
+    expect(newState.fields.btcPrice).toEqual(newPrice)
   })
 })
